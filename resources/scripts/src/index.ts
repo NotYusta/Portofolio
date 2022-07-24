bff924dce99f7963a51c4addea3ed641d128b210
unknown <xabhista19@gmail.com>
2022-07-24 17:23:21 +0700

    feat: add server port var

--- resources/scripts/src/index.ts
+++ resources/scripts/src/index.ts
@@ -2,8 +2,9 @@ import { ErrnoException } from "./typings/error";
 import app from "./app/app";
 import http from "http";
 
+const port = process.env.PORT || 1000;
 const server = http.createServer(app.initExpress());
-server.listen(1000);
+server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
 
@@ -15,10 +16,10 @@ function onError(error: ErrnoException) {
 	// handle specific listen errors with friendly messages
 	switch (error.code) {
 	case "EACCES":
-		console.error(1000 + " requires elevated privileges");
+		console.error(port + " requires elevated privileges");
 		process.exit(1);
 	case "EADDRINUSE":
-		console.error(1000 + " is already in use");
+		console.error(port + " is already in use");
 		process.exit(1);
 	default:
 		throw error;
